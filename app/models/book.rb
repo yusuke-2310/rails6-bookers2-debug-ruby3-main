0ac65f5bbdcb18c723c6ea9b2b54ce574597968f
EC2 Default User <ec2-user@ip-172-31-2-223.ap-northeast-1.compute.internal>
2022-10-02 18:36:59 +0000

    [update]いいね機能,コメント機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,18 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about"
  
   
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+     resource :favorites, only: [:create, :destroy]
+     resource :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   
 end
