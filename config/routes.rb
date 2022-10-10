0b0d4259b83b008de8aef19833aabc4f45b34c45
EC2 Default User <ec2-user@ip-172-31-2-223.ap-northeast-1.compute.internal>
2022-10-10 09:29:42 +0000

    [update]非同期化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'homes#top'
@@ -9,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
      resource :favorites, only: [:create, :destroy]
-     resource :book_comments, only: [:create, :destroy]
+     resources :book_comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:index, :show, :edit, :update] do
